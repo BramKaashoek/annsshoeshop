b07ffb0564d07739b405d1ea34ca75233f6068e1
bram <bramkaashoek@gmail.com>
2017-05-30 14:33:19 +0200

    added model testing

--- spec/models/shoe_spec.rb
+++ spec/models/shoe_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Shoe, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "associations" do
+
+  end
+
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:image) }
+    it { is_expected.to validate_presence_of(:brand) }
+    it { is_expected.to validate_presence_of(:price) }
+    it { is_expected.to validate_presence_of(:size) }
+    it { is_expected.to validate_numericality_of(:size).is_greater_than(35).is_less_than(49) }
+    it { is_expected.to validate_presence_of(:color) }
+    it { is_expected.to validate_presence_of(:material) }
+  end
 end
