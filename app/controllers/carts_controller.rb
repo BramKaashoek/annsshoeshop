d8f28e60865421a26788d3ae80152d4313d2f154
bram <bramkaashoek@gmail.com>
2017-05-31 10:09:31 +0200

    added initial working carts

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,8 +1,10 @@
 class CartsController < ApplicationController
-before_action :set_active_cart
+before_action :set_active_cart, only: [:show]
+before_action :authenticate_user!
 
   #users inactive carts, so carts that have been bought
   def index
+    @carts = Cart.where(user_id: current_user).where(active: true)
   end
 
   def show
@@ -12,20 +14,28 @@ before_action :set_active_cart
   end
 
   def create
+    @cart = current_user.carts.new(cart_params.merge(shoe_id: params[:shoe_id]))
+    @cart.save
   end
 
   def edit
   end
 
   def update
+
   end
 
   def destroy
   end
 
+  private
   #returns cart with active=true. If such a cart does not exist, create it and return it.
-  def set_active_cart
-
+  def set_active_carts
+    @carts = Cart.where(user_id: current_user_id).where(active: true)
   end
-  
+
+  def cart_params
+    params.require(:cart).permit(:user_id, :shoe_id, :active)
+  end
+
 end
