59df41c72ce7f0651f2aaa08935f7d8ea2f7b80e
annmagdalen <annmagdalen@hotmail.com>
2017-06-01 15:36:01 +0200

    Search bar works and sort-on-price button added

--- app/controllers/shoes_controller.rb
+++ app/controllers/shoes_controller.rb
@@ -4,25 +4,33 @@ class ShoesController < ApplicationController
     @cart = current_cart
     @cart.save
     @shoes = Shoe.all
-     if (params.has_key?(:cat))
+      if params[:search]
+        @shoes = Shoe.search(params[:search])
+      end
+      if (params.has_key?(:cat))
        @shoes = Shoe.joins(:categories).where(categories: { name: params[:cat] })
-     end
-     if (params.has_key?(:bra))
+      end
+      if (params.has_key?(:bra))
        @shoes = Shoe.where(brand: params[:bra])
-     end
-     if (params.has_key?(:col))
+      end
+      if (params.has_key?(:col))
        @shoes = Shoe.where(color: params[:col])
-     end
-     if (params.has_key?(:mat))
+      end
+      if (params.has_key?(:mat))
        @shoes = Shoe.where(material: params[:mat])
-     end
-     if (params.has_key?(:siz))
+      end
+      if (params.has_key?(:siz))
        @shoes = Shoe.where(size: params[:siz])
-     end
-
-     if (params.has_key?(:price_bottom))
+      end
+      if (params.has_key?(:price_bottom))
        @shoes = Shoe.where("price > ?", params[:price_bottom]).where("price <= ?", params[:price_upper])
-     end
+      end
+      if (params.has_key?(:price_low))
+       @shoes = Shoe.order(price: :asc)
+      end
+      if (params.has_key?(:price_high))
+       @shoes = Shoe.order(price: :desc)
+      end
 
     @categories = Category.all
   end
--- app/models/shoe.rb
+++ app/models/shoe.rb
@@ -2,4 +2,8 @@ class Shoe < ApplicationRecord
   has_many :shoe_categories, dependent: :destroy
   has_many :categories, through: :shoe_categories, source: :category
   has_many :cart_items
+
+  def self.search(search)
+    where("name ILIKE ? OR brand ILIKE ? OR color ILIKE ? OR material ILIKE ?", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%")
+  end
 end
